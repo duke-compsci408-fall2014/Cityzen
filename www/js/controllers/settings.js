5c0287630066d4c5f809882f9a5c5c5171c69458
Franesco Agosti <fra5@duke.edu>
2014-10-01 02:15:29 -0400

    settings page done

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -3,4 +3,24 @@
 //var app = angular.module('cityzen', ['ionic'])
 app.controller('SettingsCtrl', ['$scope', function($scope) {
 	console.log("SettingsCtrl");
+
+
+	//
+	$scope.isEditing = false;
+
+	//find notification settings:
+	$scope.notifications = false;
+
+	//retrieve firstname from state
+	$scope.first_name = "Will";
+	$scope.last_name = "Smith";
+
+	$scope.address = "505 Main, New York, NY";
+
+	$scope.toggleNotifications = function() {
+		$scope.notifications = !$scope.notifications;
+		console.log("toggling notifs");
+	}
+
+
 }]);
