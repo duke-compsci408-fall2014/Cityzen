3d77b53f40bde6f0070de42def167b7c5255d7cf
Hailey Johnson <haj5@duke.edu>
2014-10-03 13:06:26 -0400

    geolocation enabled

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -1,15 +1,17 @@
 'use strict';
 
 //var app = angular.module('cityzen', ['ionic'])
-app.controller('SettingsCtrl', ['$scope', function($scope) {
+app.controller('SettingsCtrl', function($scope, $window) {
 	console.log("SettingsCtrl");
-
 
 	//
 	$scope.isEditing = false;
 
 	//find notification settings:
 	$scope.notifications = false;
+
+	//GPS enabled?
+	$scope.geolocation - false;
 
 	//retrieve firstname from state
 	$scope.first_name = "Will";
@@ -22,5 +24,18 @@ app.controller('SettingsCtrl', ['$scope', function($scope) {
 		console.log("toggling notifs");
 	}
 
+	$scope.toggleGeolocation = function(){
+		$scope.geolocation= !$scope.geolocation;
+		console.log($scope.geolocation);
+		if($scope.geolocation){
+			$window.navigator.geolocation.getCurrentPosition(function(position){
+				console.log("latitude: " + position.coords.latitude + "\nlongitude: " + position.coords.longitude);
+			}, function(error) {
+				console.log('code: '    + error.code    + '\n' +
+	          'message: ' + error.message + '\n');
+			}, {timeout:5000});
+		}
+	}
 
-}]);
+
+});
