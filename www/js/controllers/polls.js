984ad53ef6d47a1cc82d80aa1a5ef4d99d235c4d
Franesco Agosti <fra5@duke.edu>
2014-10-19 18:45:59 -0400

    polls being displayed dynamcially

--- www/js/controllers/polls.js
+++ www/js/controllers/polls.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //var app = angular.module('cityzen', ['ionic'])
-app.controller('PollsCtrl', ['$scope', function($scope) {
-	console.log("PollsCtrl");
+app.controller('PollsCtrl', ['$scope', 'pollService', function($scope, pollService) {
+
+	$scope.polls = pollService.getAllPolls();
+
+
 }]);
