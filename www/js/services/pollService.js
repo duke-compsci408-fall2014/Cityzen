ed30602245f8059ce5c6dadf3ed06ec82a83cf0d
Franesco Agosti <fra5@duke.edu>
2014-10-20 00:38:29 -0400

    fixed trackback typo

--- www/js/services/pollService.js
+++ www/js/services/pollService.js
@@ -14,7 +14,7 @@ app.service('pollService', function($http) {
 			name: "Should LA citizens be paid to vote in city elections?",
 			description: "Some believe the incentives could help turnout, others say this would send the wrong message.",
 			customerid: 1,
-			trackbacK: "http://southerngatewaystudy.com",
+			trackback: "http://southerngatewaystudy.com",
 			socialpost: null
 		};
 		poll2 = {
@@ -22,7 +22,7 @@ app.service('pollService', function($http) {
 			name: "Gateway Improvement",
 			description: "Raleigh seeks to improve the city's Southern Gateway along South Saunders Street.  Which one of these could help most?",
 			customerid: 1,
-			trackbacK: "http://southerngatewaystudy.com",
+			trackback: "http://southerngatewaystudy.com",
 			socialpost: "727876970594007"
 		};
 		poll3 = {
@@ -30,7 +30,7 @@ app.service('pollService', function($http) {
 			name: "South Saunders Improvement",
 			description: "Should the city improve the bicycle & pedestrian experience along South Saunders Street, even if it meant traffic delays?",
 			customerid: 1,
-			trackbacK: "http://southerngatewaystudy.com",
+			trackback: "http://southerngatewaystudy.com",
 			socialpost: "727892827259088"
 		};
 		return [poll1, poll2, poll3]
