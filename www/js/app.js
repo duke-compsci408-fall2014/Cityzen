50beffc4fb165b12533059cde79570ca21452626
connorgordon <connorgordon@live.com>
2014-10-21 14:48:19 -0400

    Preparing to Link URL to Notification

--- www/js/app.js
+++ www/js/app.js
@@ -71,18 +71,29 @@ app.config(function($stateProvider, $urlRouterProvider) {
 })
 
 
-app.run(function($ionicPlatform) {
+app.run(function($rootScope, $ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+    if (window.cordova){
+      document.addEventListener("deviceready", function() {
+        window.plugin.notification.local.onclick = app.onReminderClick;
+      }, false);
+    }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
 
+app.onReminderAdd = function(id, state, json) {
+  $timeout(function() {
+    $rootScope.$broadcast('onReminderClicked', id, state, json);
+  }, 100);
+}
+
+})
 
 
